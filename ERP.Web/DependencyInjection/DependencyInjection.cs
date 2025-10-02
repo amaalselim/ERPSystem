5c0d97464ef56c4a5c35d62d323ea595e5aab0a6
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-02 16:38:39 +0300

    create injectionServices for apply in program

---
+++ ERP.Web/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace ERP.Web.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection InjectionServices(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            //
+            return services;
+        }
+    }
+}
