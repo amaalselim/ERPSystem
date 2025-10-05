1957663756f2b0c7909e2a4bd07b4a75c1752026
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:35:32 +0300

    Create Stock Configuration

---
+++ ERP.Infrastructure/Configurations/Inventory/StockConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace ERP.Infrastructure.Configurations.Inventory
+{
+    internal class StockConfiguration : IEntityTypeConfiguration<Stock>
+    {
+        public void Configure(EntityTypeBuilder<Stock> builder)
+        {
+            builder.ToTable("Stocks", "Inventory");
+
+            builder.Property(e => e.Quantity).IsRequired().HasDefaultValue(1);
+            builder.HasCheckConstraint("CK_Stock_Quantity", "Quantity >= 1 AND Quantity <= " + double.MaxValue);
+
+            builder.HasOne(e => e.product)
+                .WithMany(b => b.Stocks)
+                .HasForeignKey(e => e.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(e => e.store)
+                .WithMany(b => b.Stocks)
+                .HasForeignKey(e => e.StoreId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
