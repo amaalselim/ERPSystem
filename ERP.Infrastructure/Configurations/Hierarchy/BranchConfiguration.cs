65026b2e9b9ada07ddef8b84eeb937dc304adaf9
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:34:08 +0300

    Create Branch Configuration

---
+++ ERP.Infrastructure/Configurations/Hierarchy/BranchConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace ERP.Infrastructure.Configurations.Hierarchy
+{
+    internal class BranchConfiguration : IEntityTypeConfiguration<Branch>
+    {
+        public void Configure(EntityTypeBuilder<Branch> builder)
+        {
+            builder.ToTable("Branches", "Hierarchy");
+            builder.HasIndex(e => e.Name).IsUnique();
+            builder.Property(e => e.Name).HasMaxLength(256).IsRequired();
+            builder.Property(e => e.PhoneNumber).HasMaxLength(30).IsRequired();
+            builder.Property(e => e.Email).HasMaxLength(150).IsRequired();
+            builder.Property(e => e.Address).HasMaxLength(450).IsRequired();
+        }
+    }
+}
