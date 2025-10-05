86ce60d5b9f5f10cbc83bd58ba9436443efd3e98
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 15:23:09 +0300

    Create Interface Of UnitOfWork

---
+++ ERP.Application/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace ERP.Application.IUnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IServices<T> Repository<T>() where T : class;
+        Task<int> CompleteAsync();
+
+    }
+}
