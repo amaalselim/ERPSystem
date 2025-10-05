45b909a8ce09b04b97679dada046fe22b7e87f41
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:33:14 +0300

    Create CustomerAccount Configuration

---
+++ ERP.Infrastructure/Configurations/Accounting/CustomerAccountConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace ERP.Infrastructure.Configurations.Accounting
+{
+    internal class CustomerAccountConfiguration : IEntityTypeConfiguration<CustomerAccount>
+    {
+        public void Configure(EntityTypeBuilder<CustomerAccount> builder)
+        {
+            builder.ToTable("CustomerAccounts", "Accounting");
+
+            builder.Property(e => e.ItemId).IsRequired().HasMaxLength(256);
+            builder.Property(e => e.Debtor).HasColumnType("decimal(18,2)").HasDefaultValue(0);
+            builder.Property(e => e.Creditor).HasColumnType("decimal(18,2)").HasDefaultValue(0);
+            builder.Property(e => e.Total).HasColumnType("decimal(18,2)").HasDefaultValue(0);
+
+            builder.HasOne(e => e.Customer)
+                .WithMany(c => c.CustomerAccounts)
+                .HasForeignKey(e => e.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasCheckConstraint("CK_CustomerAccount_Debtor", "Debtor >= 0 AND Debtor <= " + double.MaxValue);
+            builder.HasCheckConstraint("CK_CustomerAccount_Creditor", "Creditor >= 0 AND Creditor <= " + double.MaxValue);
+            builder.HasCheckConstraint("CK_CustomerAccount_Total", "Total >= 0 AND Total <= " + double.MaxValue);
+
+        }
+    }
+}
