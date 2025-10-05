e4a8e867916dda00504df5be0e079316bb521be5
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 16:22:39 +0300

    ceate RoleseedServicesBL

---
+++ ERP.BL/ServicesBL/IdentityBL/RolesSeedServicesBL.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ERP.BL.ServicesBL.IdentityBL
+{
+    public class RolesSeedServicesBL(RoleManager<IdentityRole> _roleManager)
+    {
+        public async Task SeedDefaultRolesAsync()
+        {
+            if (!await _roleManager.Roles.AnyAsync(r => r.Name == ConstRoles.DefaultRole))
+                await _roleManager.CreateAsync(new IdentityRole(ConstRoles.DefaultRole));
+        }
+    }
+}
