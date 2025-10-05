2055d9278a7ab179af67d8c8de6d9b8d92f19260
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:36:47 +0300

    Create Supplier Configuration

---
+++ ERP.Infrastructure/Configurations/Purchasing/SupplierConfiguartion.cs
@@ -0,0 +1,25 @@
+﻿namespace ERP.Infrastructure.Configurations.Purchasing
+{
+    internal class SupplierConfiguration : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.ToTable("Suppliers", "Purchasing");
+
+            builder.HasIndex(e => new { e.Name, e.BranchId }).IsUnique();
+            builder.HasIndex(e => new { e.Email, e.BranchId }).IsUnique();
+            builder.HasIndex(e => new { e.PhoneNumber, e.BranchId }).IsUnique();
+
+            builder.Property(e => e.Name).IsRequired().HasMaxLength(256);
+            builder.Property(e => e.Email).IsRequired().HasMaxLength(450);
+            builder.Property(e => e.PhoneNumber).IsRequired().HasMaxLength(30);
+
+            builder.Property(e => e.FirstBalance).HasColumnType("decimal(18,2)").HasDefaultValue(0);
+
+            builder.HasOne(e => e.Branch)
+                .WithMany(c => c.Suppliers)
+                .HasForeignKey(e => e.BranchId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
