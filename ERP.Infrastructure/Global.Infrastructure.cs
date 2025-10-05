6210ff92d3d64af572e11e94e487802501ffebdf
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:42:08 +0300

    rename Global.Infrastructure

--- ERP.Infrastructure/Global.Infrastructure.cs
+++ ERP.Infrastructure/Global.Infrastructure.cs
@@ -1,6 +1,8 @@
 ﻿global using ERP.Application.IServices;
+global using ERP.Domain.Entities;
 global using ERP.Infrastructure.Data;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using System.Linq.Expressions;
