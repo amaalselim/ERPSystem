7393d54201828056012e81c95118e70d2a7f610e
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:35:44 +0300

    Create StockHistory Configuration

---
+++ ERP.Infrastructure/Configurations/Inventory/StockHistoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace ERP.Infrastructure.Configurations.Inventory
+{
+    internal class StockHistoryConfiguration : IEntityTypeConfiguration<StockHistory>
+    {
+        public void Configure(EntityTypeBuilder<StockHistory> builder)
+        {
+            builder.ToTable("StockHistories", "Inventory");
+            builder.HasOne(sh => sh.Stock)
+                   .WithMany(s => s.StockHistories)
+                   .HasForeignKey(sh => sh.StockId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+
+
+
+
+        }
+    }
+}
