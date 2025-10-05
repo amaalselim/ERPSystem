9d50ccc2278ebe27a989ca0f475a4ab26bc7d79e
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:34:41 +0300

    Create Employee Configuration

---
+++ ERP.Infrastructure/Configurations/Hierarchy/EmployeeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace ERP.Infrastructure.Configurations.Hierarchy
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.ToTable("Employees", "Hierarchy");
+            builder.HasIndex(e => new { e.Email, e.BranchId }).IsUnique();
+            builder.HasIndex(e => new { e.PhoneNumber, e.BranchId }).IsUnique();
+            builder.HasIndex(e => e.UserId).IsUnique();
+            builder.Property(e => e.FullName).HasMaxLength(256).IsRequired();
+            builder.Property(e => e.ImagePath).HasMaxLength(450).IsRequired();
+            builder.Property(e => e.Signature).HasMaxLength(450).IsRequired();
+            builder.Property(e => e.Email).HasMaxLength(150).IsRequired();
+
+            builder.HasCheckConstraint("CK_Employee_IsActive", "IsActive_IN(0,1)");
+            builder.HasCheckConstraint("CK_Employee_IsUser", "IsUser_IN(0,1)");
+
+
+
+        }
+    }
+}
