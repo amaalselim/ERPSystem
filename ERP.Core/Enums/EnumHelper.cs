d5a494560eb766cdcba43153676e55cbbaed7121
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 16:23:45 +0300

    add eNums

---
+++ ERP.Core/Enums/EnumHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace ERP.Domain.Enums
+{
+    public static class EnumHelper
+    {
+        public enum eUnit
+        {
+            peice = 1,
+            Meter,
+            Liter,
+            Gallon,
+            Unit,
+            Box,
+            KG
+        }
+        public enum eCurrency
+        {
+            EGP = 1,
+            USD,
+            KWD
+        }
+        public enum ePaymentMethod
+        {
+            Cash = 1,
+            BankTransfer,
+            Delayed
+        }
+    }
+}
