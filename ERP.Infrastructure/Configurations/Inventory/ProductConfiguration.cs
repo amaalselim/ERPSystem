96443d5e075ee941da1e0a01c6d8db3b2e10f648
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:35:23 +0300

    Create Product Configuration

---
+++ ERP.Infrastructure/Configurations/Inventory/ProductConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace ERP.Infrastructure.Configurations.Inventory
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products", "Inventory");
+
+            builder.HasIndex(e => new { e.Name, e.BranchId }).IsUnique();
+            builder.Property(e => e.Name).IsRequired().HasMaxLength(256);
+            builder.Property(e => e.Price).IsRequired().HasDefaultValue(0);
+
+            builder.HasOne(e => e.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(e => e.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+
+            builder.HasCheckConstraint("CK_Product_Price", "Price >= 0 AND Price <= " + double.MaxValue);
+
+
+
+        }
+    }
+}
