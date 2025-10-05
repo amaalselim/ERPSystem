d1793543871c7601538ff3ff2adc841678f4253a
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:37:29 +0300

    Create Currency Configuration

---
+++ ERP.Infrastructure/Configurations/Seeding/CurrencyConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace ERP.Infrastructure.Configurations.Seeding
+{
+    internal class CurrencyConfiguration : IEntityTypeConfiguration<Currency>
+    {
+        public void Configure(EntityTypeBuilder<Currency> builder)
+        {
+            builder.ToTable("Currencies", "Seeding");
+            builder.Property(e => e.Id).ValueGeneratedNever();
+            builder.HasIndex(e => e.Name).IsUnique();
+            builder.HasIndex(e => e.Code).IsUnique();
+
+            builder.Property(e => e.Name).HasMaxLength(256).IsRequired();
+            builder.Property(e => e.Code).HasMaxLength(10).HasDefaultValue("EGP");
+            builder.Property(e => e.IsActive).HasDefaultValue(true);
+
+        }
+    }
+}
