6775b6f264dd7bb617f1a3b9da25c7ae8e5c330e
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:36:23 +0300

    Create PurchaseInvoice Configuration

---
+++ ERP.Infrastructure/Configurations/Purchasing/PurchaseInvoiceConfiguration.cs
@@ -0,0 +1,40 @@
+﻿namespace ERP.Infrastructure.Configurations.Purchasing
+{
+    internal class PurchaseInvoiceConfiguration : IEntityTypeConfiguration<PurchaseInvoice>
+    {
+        public void Configure(EntityTypeBuilder<PurchaseInvoice> builder)
+        {
+            builder.ToTable("PurchaseInvoices", "Purchasing");
+
+
+            builder.Property(e => e.PurchaseInvoiceKey).IsRequired().HasMaxLength(256);
+            builder.Property(e => e.PurchaseInvoiceDate).IsRequired().HasColumnType("datetime");
+            builder.Property(e => e.SystemDate).IsRequired().HasColumnType("datetime").HasDefaultValueSql("GETDATE()");
+
+            builder.Property(e => e.BeforeDiscount).HasColumnType("decimal(18,2)").HasDefaultValue(0);
+            builder.Property(e => e.Discount).HasColumnType("decimal(18,2)").HasDefaultValue(0);
+            builder.Property(e => e.TotalAmount).HasColumnType("decimal(18,2)").HasDefaultValue(0);
+            builder.Property(e => e.TotalWritten).IsRequired().HasMaxLength(512);
+
+            builder.HasOne(e => e.Branch)
+                .WithMany(c => c.purchaseInvoices)
+                .HasForeignKey(e => e.BranchId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(e => e.supplier)
+                .WithMany(c => c.purchaseInvoices)
+                .HasForeignKey(e => e.SupplierId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(e => e.Employee)
+                .WithMany(c => c.PurchaseInvoices)
+                .HasForeignKey(e => e.EmployeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(e => e.PaymentMethod)
+                .WithMany(c => c.PurchaseInvoices)
+                .HasForeignKey(e => e.PaymentMethodId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
