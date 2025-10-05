50ed91e95ea3b8a76e84285c559193c984b8ea37
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 16:23:53 +0300

    add scoped

--- ERP.Web/DependencyInjection/DependencyInjection.cs
+++ ERP.Web/DependencyInjection/DependencyInjection.cs
@@ -12,6 +12,17 @@
                 options.UseLazyLoadingProxies().UseSqlServer(connectionString));
 
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<RolesSeedServicesBL>();
+            builder.Services.AddScoped<UnitsSeedServicesBL>();
+            builder.Services.AddScoped<CurrenciesSeedServicesBL>();
+            builder.Services.AddScoped<PaymentMehodsSeedServicesBL>();
+
+            //Seeding
+            builder.Services.AddScoped<DefaultRoles>();
+            builder.Services.AddScoped<DefaultUnits>();
+            builder.Services.AddScoped<DefaultCurrencies>();
+            builder.Services.AddScoped<DefaultPaymentMethods>();
+            builder.Services.AddScoped<DBIntializer>();
 
             return services;
         }
