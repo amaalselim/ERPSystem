1de28fce95accca2cdd50aa96b5cbbbe2868a1aa
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:34:29 +0300

    Create Department Configuration

---
+++ ERP.Infrastructure/Configurations/Hierarchy/DepartmentConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace ERP.Infrastructure.Configurations.Hierarchy
+{
+    internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.ToTable("Departments", "Hierarchy");
+            builder.HasIndex(e => new { e.Name, e.BranchId }).IsUnique();
+            builder.Property(e => e.Name).HasMaxLength(256).IsRequired();
+        }
+    }
+}
