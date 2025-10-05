2fdb45f18b0c8996e094a89b9c6c2ca8fd9d8c70
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:37:49 +0300

    Create UnitConfiguration

---
+++ ERP.Infrastructure/Configurations/Seeding/UnitConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace ERP.Infrastructure.Configurations.Seeding
+{
+    internal class UnitConfiguration : IEntityTypeConfiguration<Unit>
+    {
+        public void Configure(EntityTypeBuilder<Unit> builder)
+        {
+            builder.ToTable("Units", "Seeding");
+            builder.Property(e => e.Id).ValueGeneratedNever();
+            builder.HasIndex(e => e.Name).IsUnique();
+            builder.Property(e => e.Name).HasMaxLength(256).IsRequired();
+            builder.Property(e => e.Quantity).HasDefaultValue(1)
+                .HasColumnType("decimal(18,2)");
+
+            builder.HasCheckConstraint("CK_Unit_Quantity", "Quantity >= 1 AND Quantity <= " + double.MaxValue);
+
+
+
+        }
+    }
+}
