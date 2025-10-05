68f32bc0d33283ab81ccc0eb200a91ac8efa5531
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:36:06 +0300

    Create StoreHistory Configuration

---
+++ ERP.Infrastructure/Configurations/Inventory/StoreHistoryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace ERP.Infrastructure.Configurations.Inventory
+{
+    internal class StoreHistoryConfiguration : IEntityTypeConfiguration<StoreHistory>
+    {
+        public void Configure(EntityTypeBuilder<StoreHistory> builder)
+        {
+            builder.ToTable("StoreHistories", "Inventory");
+            builder.HasOne(sh => sh.Store)
+                   .WithMany(s => s.StoreHistories)
+                   .HasForeignKey(sh => sh.StoreId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+
+
+        }
+    }
+}
