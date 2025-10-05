c5214e565a31d21400e7736132fd6a0babe72ac7
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:34:56 +0300

    Create Brand Configuration

---
+++ ERP.Infrastructure/Configurations/Inventory/BrandConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace ERP.Infrastructure.Configurations.Inventory
+{
+    internal class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.ToTable("Brands", "Inventory");
+
+            builder.HasIndex(e => new { e.Name, e.BranchId }).IsUnique();
+            builder.Property(e => e.ImgPath).IsRequired().HasMaxLength(450);
+
+            builder.Property(e => e.ImgPath)
+                .HasDefaultValue("/Images/Users/DefaultUser.jpg");
+
+
+        }
+    }
+}
