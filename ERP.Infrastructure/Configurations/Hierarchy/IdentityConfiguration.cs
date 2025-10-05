b11c8d429dfaa84dce9434cfb6f7b3175253da22
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 05:53:06 +0300

    create identity Configuration

---
+++ ERP.Infrastructure/Configurations/Hierarchy/IdentityConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace ERP.Infrastructure.Configurations.Hierarchy
+{
+    internal class IdentityConfiguration : IEntityTypeConfiguration<IdentityUser>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUser> builder)
+        {
+            builder.ToTable("Employees", "Hierarchy");
+        }
+    }
+}
