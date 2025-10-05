bdee594a0ee3bafbdb75c845d4fe65fcc06429b1
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:33:39 +0300

    Create PaymentVochure Configuration

---
+++ ERP.Infrastructure/Configurations/Finance/PaymentVoucherConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace ERP.Infrastructure.Configurations.Finance
+{
+    internal class PaymentVoucherConfiguration : IEntityTypeConfiguration<PaymentVoucher>
+    {
+        public void Configure(EntityTypeBuilder<PaymentVoucher> builder)
+        {
+            builder.ToTable("PaymentVouchers", "Finance");
+
+            builder.Property(e => e.AmountPaid).IsRequired().HasColumnType("decimal(18,2)");
+
+
+            builder.HasOne(e => e.Employee)
+                .WithMany(a => a.PaymentVouchers)
+                .HasForeignKey(e => e.CreatedByUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(e => e.PaymentMethod)
+                .WithMany(pm => pm.PaymentVouchers)
+                .HasForeignKey(e => e.PaymentMethodId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(e => e.Supplier)
+                 .WithMany(s => s.PaymentVouchers)
+                .HasForeignKey(e => e.SupplierId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasCheckConstraint("CK_PaymentVoucher_AmountPaid", "AmountPaid >= 0 AND AmountPaid <= " + double.MaxValue);
+
+        }
+    }
+}
