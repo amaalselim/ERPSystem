ddf3c9139972cbcfb999ba0e4c9cf3e3dcadc0a4
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 03:37:41 +0300

    Create PaymentMethod Configuration

---
+++ ERP.Infrastructure/Configurations/Seeding/PaymentMethodConfiguration.cs
@@ -0,0 +1,41 @@
+﻿namespace ERP.Infrastructure.Configurations.Seeding
+{
+    internal class PaymentMethodConfiguration : IEntityTypeConfiguration<PaymentMethod>
+    {
+        public void Configure(EntityTypeBuilder<PaymentMethod> builder)
+        {
+            builder.ToTable("PaymentMethods", "Seeding");
+
+            builder.Property(e => e.Id).ValueGeneratedNever();
+
+            builder.HasIndex(e => e.Name).IsUnique();
+            builder.Property(e => e.Name).HasMaxLength(256).IsRequired();
+
+            builder.HasMany(builder => builder.SaleInvoices)
+                   .WithOne(invoice => invoice.PaymentMethod)
+                   .HasForeignKey(invoice => invoice.PaymentMethodId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(builder => builder.PurchaseInvoices)
+                     .WithOne(invoice => invoice.PaymentMethod)
+                     .HasForeignKey(invoice => invoice.PaymentMethodId)
+                     .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(builder => builder.PaymentVouchers)
+                     .WithOne(receipt => receipt.PaymentMethod)
+                     .HasForeignKey(receipt => receipt.PaymentMethodId)
+                     .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(builder => builder.ReceiptVouchers)
+                     .WithOne(receipt => receipt.PaymentMethod)
+                     .HasForeignKey(receipt => receipt.PaymentMethodId)
+                     .OnDelete(DeleteBehavior.Restrict);
+
+            //builder.HasData(
+            //    new PaymentMethod { Id = 1, Name = "Cash" },
+            //    new PaymentMethod { Id = 3, Name = "Bank Transfer" },
+            //    new PaymentMethod { Id = 2, Name = "Delayed" }
+            //);
+        }
+    }
+}
