733d3b6b92a4b180990ee193870a8e521cff5c0c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 15:23:22 +0300

    implement generic interface

---
+++ ERP.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿namespace ERP.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork(ApplicationDbContext _context) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> _repositories = new();
+        public IServices<T> Repository<T>() where T : class
+        {
+            var type = typeof(T).Name;
+            if (!_repositories.ContainsKey(type))
+            {
+                var repoInstance = new ServiceGeneric<T>(_context);
+                _repositories.Add(type, repoInstance);
+            }
+            return (IServices<T>)_repositories[type];
+        }
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
