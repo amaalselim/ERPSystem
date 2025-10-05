0102996e5d04fbc433c983fb23e8ac079234a625
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 04:29:21 +0300

    add using of DbSet

--- ERP.Infrastructure/Global.Infrastructure.cs
+++ ERP.Infrastructure/Global.Infrastructure.cs
@@ -1,8 +1,16 @@
 ﻿global using ERP.Application.IServices;
-global using ERP.Domain.Entities;
+global using ERP.Domain.Entities.Accounting;
+global using ERP.Domain.Entities.Finance;
+global using ERP.Domain.Entities.Hierarchy;
+global using ERP.Domain.Entities.Inventory;
+global using ERP.Domain.Entities.Purchasing;
+global using ERP.Domain.Entities.Sales;
+global using ERP.Domain.Entities.Seeding;
 global using ERP.Infrastructure.Data;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using System.Linq.Expressions;
+global using System.Reflection;
+
