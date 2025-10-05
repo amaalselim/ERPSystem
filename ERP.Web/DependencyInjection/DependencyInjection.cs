c96aa4dce1018c82011507124268270f92b59bc8
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-05 05:02:44 +0300

    add connection string

--- ERP.Web/DependencyInjection/DependencyInjection.cs
+++ ERP.Web/DependencyInjection/DependencyInjection.cs
@@ -1,10 +1,19 @@
-﻿namespace ERP.Web.DependencyInjection
+﻿using ERP.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP.Web.DependencyInjection
 {
     public static class DependencyInjection
     {
         public static IServiceCollection InjectionServices(this IServiceCollection services, WebApplicationBuilder builder)
         {
-            //
+            builder.Services.AddControllersWithViews();
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseLazyLoadingProxies().UseSqlServer(connectionString));
+
             return services;
         }
     }
